b672012144d94914bad56f04ebc9a1549656a9e3
ExplorerSec <gaoweixu@foxmail.com>
2025-03-29 13:23:09 +0800

    update

--- exercises/threads/threads2.rs
+++ exercises/threads/threads2.rs
@@ -7,9 +7,10 @@
 // Execute `rustlings hint threads2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 use std::sync::Arc;
+use std::sync::Mutex;
 use std::thread;
 use std::time::Duration;
 
@@ -18,13 +19,14 @@ struct JobStatus {
 }
 
 fn main() {
-    let status = Arc::new(JobStatus { jobs_completed: 0 });
+    let status = Arc::new (Mutex::new(JobStatus{jobs_completed: 0}));
     let mut handles = vec![];
     for _ in 0..10 {
         let status_shared = Arc::clone(&status);
         let handle = thread::spawn(move || {
             thread::sleep(Duration::from_millis(250));
             // TODO: You must take an action before you update a shared value
+            let mut status_shared = status_shared.lock().unwrap();
             status_shared.jobs_completed += 1;
         });
         handles.push(handle);
@@ -34,6 +36,9 @@ fn main() {
         // TODO: Print the value of the JobStatus.jobs_completed. Did you notice
         // anything interesting in the output? Do you have to 'join' on all the
         // handles?
-        println!("jobs completed {}", status.jobs_completed);
+        // 这些线程按顺序执行，但总有一个最慢的，必须等最慢的结束才能确保10个完成
+        // 同时不知道哪个最慢，或许可以while let 确保status成为10，但这样不能保证输出次数为10
+        println!("jobs completed {}", status.lock().unwrap().jobs_completed);
     }
+    
 }
