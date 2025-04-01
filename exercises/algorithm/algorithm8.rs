c7f03627f5bf3e8a84492c37441cd013ab8e69f6
ExplorerSec <gaoweixu@foxmail.com>
2025-04-01 16:14:31 +0800

    update

--- exercises/algorithm/algorithm8.rs
+++ exercises/algorithm/algorithm8.rs
@@ -2,7 +2,7 @@
 	queue
 	This question requires you to use queues to implement the functionality of the stac
 */
-// I AM NOT DONE
+// I AM DONE
 
 #[derive(Debug)]
 pub struct Queue<T> {
@@ -54,28 +54,45 @@ impl<T> Default for Queue<T> {
 
 pub struct myStack<T>
 {
-	//TODO
 	q1:Queue<T>,
 	q2:Queue<T>
 }
 impl<T> myStack<T> {
     pub fn new() -> Self {
         Self {
-			//TODO
 			q1:Queue::<T>::new(),
-			q2:Queue::<T>::new()
+			q2:Queue::<T>::new(),
         }
     }
     pub fn push(&mut self, elem: T) {
-        //TODO
+        if self.q2.size()==0 {
+            self.q2.enqueue(elem);
+            while let Ok(val) = self.q1.dequeue(){
+                self.q2.enqueue(val);
+            }
+
+        }else{
+            self.q1.enqueue(elem);
+            while let Ok(val) = self.q2.dequeue(){
+                self.q1.enqueue(val);
+            }
+        } 
     }
     pub fn pop(&mut self) -> Result<T, &str> {
-        //TODO
-		Err("Stack is empty")
+        if self.q2.size()==0{
+            match self.q1.dequeue(){
+                Ok(val) => Ok(val),
+                Err(_) => Err("Stack is empty")
+            }
+        }else{
+            match self.q2.dequeue(){
+                Ok(val) => Ok(val),
+                Err(_) => Err("Stack is empty")
+            }
+        }
     }
     pub fn is_empty(&self) -> bool {
-		//TODO
-        true
+		self.q1.size() == 0 && self.q2.size() == 0
     }
 }
 
